8f2def05bff90ee206818b705bb46cd76485da6e
Ethereal Robot <ethereal.robot@gmail.com>
2010-09-02 23:30:27 -0600

    Initial commit of boilerplate code.

---
+++ Classes/iTunesSearchHelperAppDelegate.m
@@ -0,0 +1,51 @@
+//
+//  iTunesSearchHelperAppDelegate.m
+//  iTunesSearchHelper
+//
+//  Created by Don on 9/2/10.
+//  Copyright (c) 2010 Ethereal Robot, LLC. 
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "iTunesSearchHelperAppDelegate.h"
+
+@implementation iTunesSearchHelperAppDelegate
+
+
+@synthesize window;
+
+@synthesize navigationController;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // Override point for customization after application launch.
+    // Add the navigation controller's view to the window and display.
+    [window addSubview:navigationController.view];
+    [window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+
+    // Save data if appropriate.
+}
+
+- (void)dealloc {
+
+    [window release];
+    [navigationController release];
+    [super dealloc];
+}
+
+@end
